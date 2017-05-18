b6e9e5010038df1ecefda0ae9428abf9672cd368
Danilo Weber Nunes <danilownunes@gmail.com>
2017-05-18 12:05:56 -0300

    Criacao dos esqueletos das funcoes para o calculo de derivadas e da sequencia de fibonacci e a criacao de um arquivo .rs para cada metodo.

---
+++ src/commom.rs
@@ -0,0 +1,16 @@
+/**
+ * Recebe um n que eh o comprimento da sequincia de fibonacci
+ * Retorna um vetor contendo a sequencia de fibonacci com um determinado comprimento n 
+ */
+fn get_fibo_seq(){
+
+} 
+
+/**
+ * Recebe uma string (funcao), um erro, e um ponto de dimencao n (n >=1), o valor de n, e h que eh o intervalo de 
+ * avaliacao(nesta aplicacao ele sera fixo, mas por motivos de generalizacao, sera um parametro da funcao). A funcao sempre 
+ * recebera da chamadora um h = 1000 * erro 
+ */
+fn get_derivative(){
+
+}
