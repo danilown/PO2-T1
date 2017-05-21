2a9361a166c1a20599133758634b0c6c3f15f974
Danilo Weber Nunes <danilownunes@gmail.com>
2017-05-21 16:43:01 -0300

    Implementado metodo de newton.

--- src/bissecao.rs
+++ src/bissecao.rs
@@ -1,8 +1,12 @@
+/* recebe a funcao, o limite inferior e o limite superior do intervalo e o erro.
+   Retorna o ponto onde o valor da funcao eh minimo */
 fn bissecao(func: &Fn(f64) -> f64, mut lower_bound: f64, mut upper_bound: f64, err: f64) -> f64 {
+    let mut x: f64 = (upper_bound + lower_bound) / 2.;
+    let mut derivada: f64 = get_first_derivative1(func, x, 1 as f64, err / 100.);
 
     loop {
-        let x: f64 = (upper_bound + lower_bound) / 2.;
-        let derivada = get_first_derivative1(func, x, 1 as f64, err / 100.);
+    	x = (upper_bound + lower_bound) / 2.;
+    	derivada = get_first_derivative1(func, x, 1 as f64, err / 100.);
         // A derivada no ponto eh igual a zero?
         if derivada.abs() < (err / 100.) {
             // x eh ponto de minimo
--- src/newton.rs
+++ src/newton.rs
@@ -1,3 +1,24 @@
-fn newton(func: &Fn(f64) -> f64, mut lower_bound: f64, mut upper_bound: f64, intervalo_incerteza: f64) -> f64 {
+/* recebe a funcao, o limite inferior e o limite superior do intervalo e o erro.
+   Retorna o ponto onde o valor da funcao eh minimo 
+   neste metodo x inicial (chute inicial) eh definido como lower_bound */
+fn newton(func: &Fn(f64) -> f64, lower_bound: f64, upper_bound: f64, err: f64) -> f64 {
+    let mut x_atual: f64 = lower_bound; // xk
+    let mut x_prox: f64; // xk+1
+    // derivada primeira no ponto xk
+    let mut derivada_primera: f64;
+    // derivada segunda no ponto xk
+    let mut derivada_segunda: f64;
 
+    loop {
+        derivada_primera = get_first_derivative1(func, x_atual, 1 as f64, err / 100.);
+        derivada_segunda = get_second_derivative1(func, x_atual, 1 as f64, err / 100.);
+        x_prox = x_atual - (derivada_primera / derivada_segunda);
+        /* se a derivada primera da funcao no ponto atual eh menor que o erro aceitavel ou 
+           se o modulo da diferenca entre o x anterior e o x atual dividido pelo maior entre 1 e o ponto atual eh menor que o erro aceitavel
+        */
+        if (get_first_derivative1(func, x_prox, 1 as f64, err / 100.).abs() < err) || ((x_prox - x_atual).abs() / x_prox.max(1.) < err) {
+            return x_prox;
+        }
+        x_atual = x_prox;
+    }
 }
